741df94b7801fe3c42e11622b9073966b4aed0cf
felipeemiliano <111158344+felipeemiliano@users.noreply.github.com>
2022-09-06 20:31:22 -0300

    Create teste-node.js

---
+++ teste-node.js
@@ -0,0 +1,18 @@
+
+  "name": "teste-node",
+  "version": "1.0.0",
+  "description": "aula softex",
+  "main": "index.js",
+  "scripts": {
+    "test": "teste-node-index.js"
+  },
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/felipeemiliano/Media-alunos/blob/main/media2.js"
+  },
+  "keywords": [
+    "package"
+  ],
+  "author": "Felipe emiliano",
+  "license": "ISC"
+}
